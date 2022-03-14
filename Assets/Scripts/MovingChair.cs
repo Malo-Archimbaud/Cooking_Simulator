d807f839dfd84fa2fb1f7b1c9f2c69616d1df428
Malo Archimbaud <malo.archimbaud@algosup.com>
2022-03-14 11:22:17 +0100

    Update

---
+++ Assets/Scripts/MovingChair.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingChair : MonoBehaviour
+{
+    public Animator MoveChair;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+            MoveChair.SetBool("Move", true);
+        if (Input.GetKey(KeyCode.R))
+            MoveChair.SetBool("Move", false);
+    }
+}
