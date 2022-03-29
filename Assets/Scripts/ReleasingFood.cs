afe08943efb3e9a0e72037a691d28bddca2a59c3
Malo Archimbaud <malo.archimbaud@algosup.com>
2022-03-29 10:36:52 +0200

    update

--- Assets/Scripts/ReleasingFood.cs
+++ Assets/Scripts/ReleasingFood.cs
@@ -6,6 +6,7 @@ public class ReleasingFood : MonoBehaviour
 {
     private GameObject Food;
     public GameObject Hand;
+    public GameObject CuttingBoard;
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +25,18 @@ public class ReleasingFood : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.R) && Hand.transform.childCount > 0)
         {
-            Food.transform.SetParent(null);
-            Food.transform.transform.position = new Vector3(-14.24f, 1.428f, 25.208f);
-            Food = null;    
+            if (Hand.transform.Find("tomato"))
+            {
+                Food.transform.SetParent(CuttingBoard.transform);
+                Food.transform.transform.localPosition = new Vector3(0,0,0);
+                Food = null;
+            }
+            else if (Hand.transform.childCount > 0) 
+                    {
+                        Food.transform.SetParent(null);
+                        Food.transform.transform.position = new Vector3(-14.24f, 1.428f, 25.208f);
+                        Food = null;
+                    }
         }
     }
 }
